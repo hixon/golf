21c5e506629520ec3c1c1d60a710a02b2ce86510
hixon <ryan.hixon@gmail.com>
2022-06-13 21:16:37 -0400

    removing pro shop money since everyone pays inside

--- golf-tracker/src/App.js
+++ golf-tracker/src/App.js
@@ -76,11 +76,8 @@ const Skins = (props) => {
   if(props.stats != undefined && props.stats.length > 0){
     const totalNumberOfGolfers = props.stats.filter(item => item.Active == 1).length;
 
-    //total money = number of golfers * 25 (15 for golf 10 for gambling)
-    const proshopMoney = 20;
+    //total money = number of golfers * 10 (for gambling)
     const gamblingMoney = 10;
-    const totalMoney = totalNumberOfGolfers * (proshopMoney + gamblingMoney);
-    const toTerry = totalNumberOfGolfers * proshopMoney;
     const toSkins = totalNumberOfGolfers * (gamblingMoney / 2);
     const toTeams = totalNumberOfGolfers * (gamblingMoney / 2);
 
@@ -145,13 +142,8 @@ const Skins = (props) => {
               <td>Blind Teams: ${toTeams} ({totalNumberOfGolfers} * ${gamblingMoney/2})</td>
             </tr>
             <tr>
-              <td>Total money: ${totalMoney} ({totalNumberOfGolfers} * ${proshopMoney} + ${gamblingMoney})</td>
               <td>Per Skin: {perSkin} (${toSkins} / {SkinDetails.length})</td>
               <td>1st: ${firstTeam} (60% of ${toTeams})</td>
-            </tr>
-            <tr>
-              <td>Terry: ${toTerry} ({totalNumberOfGolfers} * ${proshopMoney})</td>
-              <td></td>
               <td>2nd: ${secondTeam} (40% of ${toTeams})</td>
             </tr>
           </tbody>
